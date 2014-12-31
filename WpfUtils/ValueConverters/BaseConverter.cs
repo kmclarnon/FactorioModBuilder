0bf800f3b1e4fa53f6cb10a7d1e0afebb1881488
Kevin McLarnon <mclarnon.k@gmail.com>
2014-12-31 10:33:45 -0500

    Added BaseConverter and BooleanNot converter which derive from MarkupExtension to allow for Converter={x:Converter} syntax in bindings

---
+++ WpfUtils/ValueConverters/BaseConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WpfUtils.ValueConverters
+{
+    public abstract class BaseConverter : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
