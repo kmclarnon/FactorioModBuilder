bbc5746f4cf4a08a8fbf09b04010de267c5ddcac
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-01 00:20:08 -0500

    Added PossibleSubgroups property to the Prototypes view model

--- FactorioModBuilder/ViewModels/ProjectItems/PrototypesVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/PrototypesVM.cs
@@ -1,6 +1,7 @@
 ﻿using FactorioModBuilder.Models.ProjectItems;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,22 @@ namespace FactorioModBuilder.ViewModels.ProjectItems
 {
     public class PrototypesVM : ProjectItemVM
     {
+        public ObservableCollection<String> PossibleSubgroups
+        {
+            get
+            {
+                ProjectItemVM res;
+                if (!this.TryFindElementWithPropertyDown(typeof(ObservableCollection<String>),
+                    "SubgroupNames", out res))
+                {
+                    throw new Exception("Could not find appropriate child element to populate Possible Subgroups");
+                }
+
+                return (ObservableCollection<String>)res.GetType()
+                    .GetProperty("SubgroupNames").GetValue(res);
+            }
+        }
+
         public PrototypesVM(ProjectItemVM parent, Prototypes types)
             : base(parent, types)
         {
--- FactorioModBuilder/ViewModels/ProjectItems/SubGroupsVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/SubGroupsVM.cs
@@ -1,6 +1,7 @@
 ﻿using FactorioModBuilder.Models.ProjectItems;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,14 @@ namespace FactorioModBuilder.ViewModels.ProjectItems
 {
     public class SubGroupsVM : ProjectItemVM
     {
+        public ObservableCollection<String> SubgroupNames { get; set; }
+
         public SubGroupsVM(ProjectItemVM parent, SubGroups items)
             : base(parent, items)
         {
+            this.SubgroupNames = new ObservableCollection<string>();
+            this.SubgroupNames.Add("Sg 1");
+            this.SubgroupNames.Add("Sg 2");
         }
     }
 }
