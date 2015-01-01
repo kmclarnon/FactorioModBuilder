a0aac929654d5dc993466646b3132cdfa4aba43f
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-01 16:31:37 -0500

    Implemented basic item group screen and item group

--- FactorioModBuilder/Models/ProjectItems/Groups.cs
+++ FactorioModBuilder/Models/ProjectItems/Groups.cs
@@ -8,6 +8,12 @@ namespace FactorioModBuilder.Models.ProjectItems
 {
     public class Groups : ProjectItem
     {
+        public string Type { get; set; }
+        public string IconPath { get; set; }
+        public string InvOrder { get; set; }
+        public string Order { get; set; }
+        public bool Enabled { get; set; }
+
         public Groups() : base("Groups")
         {
         }
