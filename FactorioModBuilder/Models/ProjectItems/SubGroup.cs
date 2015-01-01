765868c2434776e752abf8ab29bc50298016595e
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-01 16:29:29 -0500

    Implemented Basic Subgroup and associated view model

---
+++ FactorioModBuilder/Models/ProjectItems/SubGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Models.ProjectItems
+{
+    public class SubGroup : ProjectItem
+    {
+        public string Type { get; set; }
+        public string Group { get; set; }
+        public string Order { get; set; }
+
+        public SubGroup(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ FactorioModBuilder/ViewModels/ProjectItems/SubGroupVM.cs
@@ -0,0 +1,58 @@
+﻿using FactorioModBuilder.Models.ProjectItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.ViewModels.ProjectItems
+{
+    public class SubGroupVM : ProjectItemVM
+    {
+        public string Type
+        {
+            get { return _internal.Type; }
+            set
+            {
+                if(_internal.Type != value)
+                {
+                    _internal.Type = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Group
+        {
+            get { return _internal.Group; }
+            set
+            {
+                if(_internal.Group != value)
+                {
+                    _internal.Group = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Order
+        {
+            get { return _internal.Order; }
+            set
+            {
+                if(_internal.Order != value)
+                {
+                    _internal.Order = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private SubGroup _internal { get { return (SubGroup)_item; } }
+
+        public SubGroupVM(ProjectItemVM parent, SubGroup item)
+            : base(parent, item)
+        {
+        }
+    }
+}
