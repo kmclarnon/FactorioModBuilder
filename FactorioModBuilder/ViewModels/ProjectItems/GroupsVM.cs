3a6612939cf45aab0ff7cd07cb0c0aa8b9d683fd
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-02 15:32:00 -0500

    Added add and remove commands to the group and subgroup view models

--- FactorioModBuilder/ViewModels/ProjectItems/GroupsVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/GroupsVM.cs
@@ -1,17 +1,67 @@
 ﻿using FactorioModBuilder.Models.ProjectItems;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfUtils;
 
 namespace FactorioModBuilder.ViewModels.ProjectItems
 {
     public class GroupsVM : ProjectItemVM
     {
+        public ObservableCollection<GroupVM> ItemList { get; private set; }
+
+        private ICommand _addGroupCmd;
+        public ICommand AddGroupCmd
+        {
+            get
+            {
+                if (_addGroupCmd == null)
+                    _addGroupCmd = new RelayCommand(
+                        (x => this.AddGroup()), (x => this.CanAddGroup()));
+                return _addGroupCmd;
+            }
+        }
+
+        private ICommand _removeGroupCmd;
+        public ICommand RemoveGroupCmd
+        {
+            get
+            {
+                if (_removeGroupCmd == null)
+                    _removeGroupCmd = new RelayCommand(
+                        (x => this.RemoveGroup()), (x => this.CanRemoveGroup()));
+                return _removeGroupCmd;
+            }
+        }
+
         public GroupsVM(ProjectItemVM parent, Groups groups)
             : base(parent, groups)
-        { 
+        {
+            this.ItemList = new ObservableCollection<GroupVM>();
+        }
+
+        private bool CanAddGroup()
+        {
+            return true;
+        }
+
+        private void AddGroup()
+        {
+            
+        }
+
+        private bool CanRemoveGroup()
+        {
+            return true;
+        }
+
+        private void RemoveGroup()
+        {
+
         }
     }
 }
--- FactorioModBuilder/ViewModels/ProjectItems/SubGroupsVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/SubGroupsVM.cs
@@ -5,17 +5,63 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfUtils;
 
 namespace FactorioModBuilder.ViewModels.ProjectItems
 {
     public class SubGroupsVM : ProjectItemVM
     {
-        public ObservableCollection<SubGroupVM> SubgroupList { get; private set; }
+        public ObservableCollection<SubGroupVM> ItemList { get; private set; }
+
+        private ICommand _addSubgroupCmd;
+        public ICommand AddSubgroupCmd
+        {
+            get
+            {
+                if (_addSubgroupCmd == null)
+                    _addSubgroupCmd = new RelayCommand(
+                        (x => this.AddSubgroup()), (x => this.CanAddSubgroup()));
+                return _addSubgroupCmd;
+            }
+        }
+
+        private ICommand _removeSubgroupCmd;
+        public ICommand RemoveSubgroupCmd
+        {
+            get
+            {
+                if (_removeSubgroupCmd == null)
+                    _removeSubgroupCmd = new RelayCommand(
+                        (x => this.RemoveSubgroup()), (x => this.CanRemoveSubgroup()));
+                return _removeSubgroupCmd;
+            }
+        }
 
         public SubGroupsVM(ProjectItemVM parent, SubGroups items)
             : base(parent, items)
         {
-            this.SubgroupList = new ObservableCollection<SubGroupVM>();
+            this.ItemList = new ObservableCollection<SubGroupVM>();
+        }
+
+        private bool CanAddSubgroup()
+        {
+            return true;
+        }
+
+        private void AddSubgroup()
+        {
+
+        }
+
+        private bool CanRemoveSubgroup()
+        {
+            return true;
+        }
+
+        private void RemoveSubgroup()
+        {
+
         }
     }
 }
