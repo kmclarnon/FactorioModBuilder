289e5111962c624b8c2d713c0c6dc21143c0b575
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-02 22:43:23 -0500

    Implemented NullImage converter to solve the null source data errors encountered by image elements of new groups

---
+++ WpfUtils/ValueConverters/NullImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfUtils.ValueConverters
+{
+    public class NullImage : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
