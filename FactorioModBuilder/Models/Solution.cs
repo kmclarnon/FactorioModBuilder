00f8e1c0625dfdc4e97232bc266616dcf0ceacd6
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-06 14:24:19 -0500

    Basic implementation of Solution and SolutionVM

---
+++ FactorioModBuilder/Models/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUtils;
+
+namespace FactorioModBuilder.Models
+{
+    public class Solution
+    {
+        public List<Project> Projects { get; private set; }
+        public string Name { get; set; }
+
+        public Solution(string name, Project p)
+        {
+            this.Name = name;
+            this.Projects = new List<Project>();
+        }
+
+        public Solution(string name, IEnumerable<Project> projects)
+        {
+            this.Name = name;
+            this.Projects = projects.ToList();
+        }
+    }
+}
---
+++ FactorioModBuilder/ViewModels/SolutionItems/SolutionVM.cs
@@ -0,0 +1,40 @@
+﻿using FactorioModBuilder.Models;
+using FactorioModBuilder.ViewModels.ProjectItems;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUtils;
+
+namespace FactorioModBuilder.ViewModels
+{
+    public class SolutionVM : BaseVM
+    {
+        public ObservableCollection<ProjectVM> Projects { get; private set; }
+        
+        public string Name
+        {
+            get { return _sol.Name; }
+            set
+            {
+                if(_sol.Name != value)
+                {
+                    _sol.Name = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private Solution _sol;
+
+        public SolutionVM(Solution sol)
+        {
+            this.Projects = new ObservableCollection<ProjectVM>();
+            _sol = sol;
+            foreach (var p in _sol.Projects)
+                this.Projects.Add(new ProjectVM(p));
+        }
+    }
+}
