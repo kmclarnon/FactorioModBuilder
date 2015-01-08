a0086a7aa18a01e2b1e3608a6014f569674c5bd3
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-08 11:26:55 -0500

    Implemented ICompilerSource interface to provide the compiler with information

---
+++ FactorioModBuilder/Build/ICompilerSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build
+{
+    public interface ICompilerSource
+    {
+        Dictionary<String, CompileUnit> CompileData { get; }
+    }
+}
