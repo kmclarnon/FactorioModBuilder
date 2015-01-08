898a2e137e573d4109caf9704710074df239b489
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-08 17:28:33 -0500

    Added the data and control lua file compiler extension

---
+++ FactorioModBuilder/Build/Extensions/ControlLuaExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build.Extensions
+{
+    public class ControlLuaExtension : ExtensionBase
+    {
+        public override string SupportedUnitName
+        {
+            get { return "control.lua"; }
+        }
+
+        public override bool SeparateFile
+        {
+            get { return true; }
+        }
+
+        public override string Filename
+        {
+            get { return "control.lua"; }
+        }
+
+        public override bool BuildUnit(CompileUnit unit, out string result)
+        {
+            result = "";
+            return false;
+        }
+    }
+}
---
+++ FactorioModBuilder/Build/Extensions/DataLuaExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build.Extensions
+{
+    public class DataLuaExtension : ExtensionBase
+    {
+        public override string SupportedUnitName
+        {
+            get { return "data.lua"; }
+        }
+
+        public override bool SeparateFile
+        {
+            get { return true; }
+        }
+
+        public override string Filename
+        {
+            get { return "data.lua"; }
+        }
+
+        public override bool BuildUnit(CompileUnit unit, out string result)
+        {
+            result = "";
+            return false;
+        }
+    }
+}
