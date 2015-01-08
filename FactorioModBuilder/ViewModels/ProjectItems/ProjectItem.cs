7c077a9b4429c2c1f813fead5b49ca6c86c576d3
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-08 11:26:24 -0500

    Implemented common base class for all project items

---
+++ FactorioModBuilder/ViewModels/ProjectItems/ProjectItem.cs
@@ -0,0 +1,36 @@
+﻿using FactorioModBuilder.Build;
+using FactorioModBuilder.Models.Base;
+using FactorioModBuilder.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.ViewModels.ProjectItems
+{
+    public abstract class ProjectItem<T> : TreeItemVM<T>, ICompilerSource where T : TreeItem<T>
+    {
+        public abstract Dictionary<string, CompileUnit> CompileData { get; }
+
+        public ProjectItem(T item) 
+            : base(item)
+        {
+        }
+
+        public ProjectItem(TreeItemVMBase parent, T item)
+            : base(parent, item)
+        {
+        }
+
+        public ProjectItem(T item, IEnumerable<TreeItemVMBase> children)
+            : base(item, children)
+        {
+        }
+
+        public ProjectItem(TreeItemVMBase parent, T item, IEnumerable<TreeItemVMBase> childre)
+            : base(parent, item, childre)
+        {
+        }
+    }
+}
