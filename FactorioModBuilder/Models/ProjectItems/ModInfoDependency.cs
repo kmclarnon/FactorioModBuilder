aee2cf0f94762fabb72bd403062ff684ed2bc822
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-12 15:14:45 -0500

    Beginning implementation to support mod dependencies as per Issue #13

---
+++ FactorioModBuilder/Models/ProjectItems/ModInfoDependency.cs
@@ -0,0 +1,22 @@
+﻿using FactorioModBuilder.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Models.ProjectItems
+{
+    public class ModInfoDependency : TreeItem<ModInfoDependency>
+    {
+        public bool Optional { get; set; }
+        public bool Enabled { get; set; }
+        public string Version { get; set; }
+
+        public ModInfoDependency(string name)
+            : base(name)
+        {
+            this.Enabled = true;
+        }
+    }
+}
---
+++ FactorioModBuilder/ViewModels/ProjectItems/ModInfoDependencyVM.cs
@@ -0,0 +1,50 @@
+﻿using FactorioModBuilder.Build;
+using FactorioModBuilder.Models.ProjectItems;
+using FactorioModBuilder.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.ViewModels.ProjectItems
+{
+    public class ModInfoDependencyVM : ProjectItem<ModInfoDependency, ModInfoDependencyVM>
+    {
+        public override CompileUnit CompilerData
+        {
+            get
+            {
+
+            }
+        }
+
+        public bool Optional
+        {
+            get { return _internal.Optional; }
+            set { this.SetProperty(_internal, value); }
+        }
+
+        public bool Enabled
+        {
+            get { return _internal.Enabled; }
+            set { this.SetProperty(_internal, value); }
+        }
+
+        public string Version
+        {
+            get { return _internal.Version; }
+            set { this.SetProperty(_internal, value); }
+        }
+
+        public ModInfoDependencyVM(ModInfoDependency item)
+            : base(item)
+        {
+        }
+
+        public ModInfoDependencyVM(TreeItemVMBase parent, ModInfoDependency item)
+            : base(parent, item)
+        {
+        }
+    }
+}
