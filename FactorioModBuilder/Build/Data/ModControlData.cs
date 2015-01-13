4c111d0ccfe6e0e82e8d41d1b0cb23f970ee4593
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-13 11:23:14 -0500

    Implemented basic locale, modcontrol and mod data data units

---
+++ FactorioModBuilder/Build/Data/ModControlData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build.Data
+{
+    public class ModControlData : DataUnit
+    {
+        public ModControlData()
+            : base(Extensions.ExtensionType.FactorioControl)
+        {
+        }
+    }
+}
---
+++ FactorioModBuilder/Build/Data/ModDataData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build.Data
+{
+    public class ModDataData : DataUnit
+    {
+        public ModDataData()
+            : base(Extensions.ExtensionType.FactorioData)
+        {
+        }
+    }
+}
