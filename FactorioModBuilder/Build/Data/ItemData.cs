c88628fd792c6cb75c2d5e7664d679a6def42aed
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-15 13:27:07 -0500

    Added a DataUnit to represent Item data

---
+++ FactorioModBuilder/Build/Data/ItemData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Build.Data
+{
+    public class ItemData : DataUnit
+    {
+        public string Name { get; private set; }
+        public string Icon { get; private set; }
+        public string SubGroup { get; private set; }
+        public string Order { get; private set; }
+        public string PlaceResult { get; private set; }
+        public int StackSize { get; private set; }
+
+        public ItemData(string name, string icon, string subgroup,
+            string order, string placeResult, int stackSize)
+            : base(Extensions.ExtensionType.PrototypeItems)
+        {
+            this.Name = name;
+            this.Icon = icon;
+            this.SubGroup = subgroup;
+            this.Order = order;
+            this.PlaceResult = placeResult;
+            this.StackSize = StackSize;
+        }
+    }
+}
