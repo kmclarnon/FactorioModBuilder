a302b5b5c95dbc6b8e97fa601e6ce09ececb10a0
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-15 11:39:26 -0500

    Implemented some convenience extension methods

---
+++ FactorioModBuilder/Extensions/CollectionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Extensions
+{
+    public static class CollectionMethods
+    {
+        public static List<T> ToList<T>(this T item)
+        {
+            return new List<T>() { item };
+        }
+
+        public static IEnumerable<T> ConcatMany<T>(this IEnumerable<T> target, params IEnumerable<T>[] elms)
+        {
+            foreach (var e in elms)
+                target = target.Concat(e);
+            return target;
+        }
+    }
+}
