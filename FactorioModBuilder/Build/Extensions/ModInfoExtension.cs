0c195564168f7ba153d95e5a2294f39dfdc38b2d
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-17 17:07:26 -0500

    Implemented new Info.json writer in ModInfoExtension (Issue #28)

--- FactorioModBuilder/Build/Extensions/ModInfoExtension.cs
+++ FactorioModBuilder/Build/Extensions/ModInfoExtension.cs
@@ -18,7 +18,26 @@ namespace FactorioModBuilder.Build.Extensions
 
         protected override bool BuildUnit(IEnumerable<ModInfoData> units, StreamWriter sw)
         {
-            var unit = units.Single();
+            var info = units.Single();
+
+            if (!Regex.IsMatch(info.Version, @"^\d{1,4}\.\d{1,4}\.\d{1,4}$"))
+                return false;
+            if (!(info.Name == this.ProjectName || info.Name + "_" + info.Version == this.ProjectName))
+                return false;
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine("  \"name\" : \"" + info.Name + "\",");
+            sb.AppendLine("  \"version\" : \"" + info.Version + "\",");
+            sb.AppendLine("  \"title\" : \"" + info.Author + "\",");
+            sb.AppendLine("  \"contact\" : \"" + info.Contact + "\",");
+            sb.AppendLine("  \"homepage\" : \"" + info.Homepage + "\",");
+            sb.AppendLine("  \"description\" : \"" + info.Description + "\",");
+            sb.AppendLine("  \"dependencies\" : " + this.DependencyString);
+            sb.AppendLine("}");
+
+            sw.Write(sb.ToString());
+            return true;
         }
 
         protected override bool GetOutputPath(out string path)
