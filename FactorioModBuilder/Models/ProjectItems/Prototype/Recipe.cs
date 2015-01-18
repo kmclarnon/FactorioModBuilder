db7370862e408f571071c8a9cca55394cfc9c8a1
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-18 12:08:14 -0500

    Added data properties to the recipe class

--- FactorioModBuilder/Models/ProjectItems/Prototype/Recipe.cs
+++ FactorioModBuilder/Models/ProjectItems/Prototype/Recipe.cs
@@ -9,6 +9,12 @@ namespace FactorioModBuilder.Models.ProjectItems.Prototype
 {
     public class Recipe : TreeItem<Recipe>
     {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+        public int EnergyRequired { get; set; }
+        public string Result { get; set; }
+        public int ResultCount { get; set; }
+
         public Recipe(string name) : base(name)
         {
 
