56c5598f073c29c867ad2b4287d9d7f9e1d18ad3
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-19 18:17:21 -0500

    Implemented image copying in the graphics extension

--- FactorioModBuilder/Build/Extensions/GraphicsExtension.cs
+++ FactorioModBuilder/Build/Extensions/GraphicsExtension.cs
@@ -18,10 +18,37 @@ namespace FactorioModBuilder.Build.Extensions
 
         protected override bool BuildUnit(IEnumerable<GraphicsData> units, StreamWriter sr)
         {
-            string baseDir = "__" + this.ProjectName + "__";
-            foreach(var g in units)
-                this.GraphicsPathLookup.Add(g.ImportPath, baseDir + "/" + g.ExportPath);
-            return true;
+            try
+            {
+                string baseDir = "__" + this.ProjectName + "__";
+                foreach (var g in units)
+                {
+                    if (!this.GraphicsPathLookup.ContainsKey(g.ImportPath))
+                    {
+                        FileInfo info = new FileInfo(g.ImportPath);
+                        if (!info.Exists)
+                            return false;
+
+                        // modify the path and copy the image into the appropriate location
+                        this.GraphicsPathLookup.Add(g.ImportPath, baseDir + "/" + g.ExportPath);
+
+                        string dirPath = Path.Combine(this.TemporaryDirectory, Path.GetDirectoryName(g.ExportPath));
+                        DirectoryInfo dinfo = new DirectoryInfo(dirPath);
+                        if (!dinfo.Exists)
+                            Directory.CreateDirectory(dirPath);
+
+                        info.CopyTo(Path.Combine(dirPath, Path.GetFileName(g.ExportPath)));
+                    }
+                    else
+                        return false;
+                }
+
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
         }
     }
 }
