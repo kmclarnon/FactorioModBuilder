7e361892a5f460719b566e2c3d6e69c81279a6a1
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-20 17:12:48 -0500

    More documentation (Issue #22)

--- FactorioModBuilder/ViewModels/ProjectItems/ProjectItem.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/ProjectItem.cs
@@ -10,28 +10,60 @@ using System.Threading.Tasks;
 
 namespace FactorioModBuilder.ViewModels.ProjectItems
 {
-    public abstract class ProjectItem<TItem, TViewModel> : TreeItemVM<TItem, TViewModel>, ICompilerSource 
-        where TItem : TreeItem<TItem>
-        where TViewModel : TreeItemVM<TItem, TViewModel>
+    /// <summary>
+    /// The base class for project items.  All project items implement ICompilerSource so thate
+    /// they can provide information to the compiler in a consisten manner
+    /// </summary>
+    /// <typeparam name="TModel">The type of the model to wrap</typeparam>
+    /// <typeparam name="TViewModel">The type of the this view model (CRTP)</typeparam>
+    public abstract class ProjectItem<TModel, TViewModel> : TreeItemVM<TModel, TViewModel>, ICompilerSource 
+        where TModel : TreeItem<TModel>
+        where TViewModel : TreeItemVM<TModel, TViewModel>
     {
+        /// <summary>
+        /// Provides a list of DataUnits generated from the data contained in the associated view models
+        /// </summary>
         public abstract IEnumerable<DataUnit> CompilerData { get; }
 
-        public ProjectItem(TItem item) 
+        /// <summary>
+        /// The basic constructor to wrap the given model in its associated view model
+        /// </summary>
+        /// <param name="item"></param>
+        public ProjectItem(TModel item) 
             : base(item)
         {
         }
 
-        public ProjectItem(TreeItemVMBase parent, TItem item)
+        /// <summary>
+        /// The constructor to wrap the given model in the appropriate view model
+        /// and associate it with the given parent
+        /// </summary>
+        /// <param name="parent">The parent view model</param>
+        /// <param name="item">The model to wrap</param>
+        public ProjectItem(TreeItemVMBase parent, TModel item)
             : base(parent, item)
         {
         }
 
-        public ProjectItem(TItem item, IEnumerable<TreeItemVMBase> children)
+        /// <summary>
+        /// The constructor to wrap the given model in the appropriate view model
+        /// and give it the provided children
+        /// </summary>
+        /// <param name="item">The model to wrap</param>
+        /// <param name="children">The view model children of this model</param>
+        public ProjectItem(TModel item, IEnumerable<TreeItemVMBase> children)
             : base(item, children)
         {
         }
 
-        public ProjectItem(TreeItemVMBase parent, TItem item, IEnumerable<TreeItemVMBase> childre)
+        /// <summary>
+        /// The constructor to wrap the given model in the appropriate view model,
+        /// give it the provided children and associate it with the given parent
+        /// </summary>
+        /// <param name="parent">The parent view model</param>
+        /// <param name="item">The model to wrap</param>
+        /// <param name="childre">The view model children of this model</param>
+        public ProjectItem(TreeItemVMBase parent, TModel item, IEnumerable<TreeItemVMBase> childre)
             : base(parent, item, childre)
         {
         }
--- FactorioModBuilder/ViewModels/SolutionItems/SolutionVM.cs
+++ FactorioModBuilder/ViewModels/SolutionItems/SolutionVM.cs
@@ -13,20 +13,37 @@ using WpfUtils;
 
 namespace FactorioModBuilder.ViewModels
 {
+    /// <summary>
+    /// The view model used to wrap the solution model
+    /// </summary>
     public class SolutionVM : TreeItemVM<Solution, SolutionVM>
-    {      
+    {   
+        /// <summary>
+        /// The constructor used to wrap the given Solution model in a view model and associate
+        /// it with the given project children.
+        /// </summary>
+        /// <param name="sol"></param>
+        /// <param name="projects"></param>
         public SolutionVM(Solution sol, IEnumerable<ProjectVM> projects)
             : base(sol, projects)
         {
             this.Children.CollectionChanged += Children_CollectionChanged;
+            // As the SolutionVM is the top level of the TreeView, it is appropriate to call Init complete
+            // at this point since there is no further construction to perform
             this.InitComplete();
         }
 
-        void Children_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        /// <summary>
+        /// Handles the events thrown from the addition or removal of children (projects) to this solution view model
+        /// </summary>
+        private void Children_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
             this.NotifyPropertyChanged("Name");
         }
 
+        /// <summary>
+        /// Modifies the solution name to from "name" to "Solution 'name' (# projects)"
+        /// </summary>
         protected override string OnGetName(string name)
         {
             var res = "Solution '" + name + "' (" + this.Children.Count + " ";
