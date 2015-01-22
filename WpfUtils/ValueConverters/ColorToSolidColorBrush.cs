f12f089182a4e0434995c3db2e16ddfa784fa5ab
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-22 16:52:12 -0500

    Added Color converter to the WpfUtils

---
+++ WpfUtils/ValueConverters/ColorToSolidColorBrush.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfUtils.ValueConverters
+{
+    public class ColorToSolidColorBrush : BaseConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            if (value is Color)
+            {
+                Color color = (Color)value;
+                return new SolidColorBrush(color);
+            }
+
+            Type type = value.GetType();
+            throw new InvalidOperationException("Unsupported type [" + type.Name + "]");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value == null)
+                return null;
+
+            SolidColorBrush brush = value as SolidColorBrush;
+            if (brush == null)
+                throw new InvalidOperationException("Unsupported type [" + value.GetType().Name + "]");
+            return brush.Color;
+        }
+    }
+}
