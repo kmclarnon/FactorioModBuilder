6137cce09c0f31cc50e3d1b0f18320abb45561e6
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-22 12:18:48 -0500

    Implemented basic fluid screen view model

--- FactorioModBuilder/ViewModels/ProjectItems/Prototype/FluidsVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/Prototype/FluidsVM.cs
@@ -2,22 +2,56 @@
 using FactorioModBuilder.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FactorioModBuilder.ViewModels.ProjectItems.Prototype
 {
     public class FluidsVM : TreeItemVM<Fluids, FluidsVM>
     {
+        public ObservableCollection<FluidVM> ItemList { get; set; }
+
+        public ICommand AddFluidCmd { get { return this.GetCommand(this.AddFluid, this.CanAddFluid); } }
+        public ICommand RemoveFluidCmd { get { return this.GetCommand(this.RemoveFluid, this.CanRemoveFluid); } }
+
+        private int _newCount = 1;
+
         public FluidsVM(Fluids fl) 
-            : base(fl)
+            : this(null, fl)
         {
         }
 
         public FluidsVM(TreeItemVMBase parent, Fluids fl)
             : base(parent, fl)
         {
+            this.ItemList = new ObservableCollection<FluidVM>();
+        }
+
+        private bool CanAddFluid()
+        {
+            return true;
+        }
+
+        private void AddFluid()
+        {
+            this.ItemList.Add(new FluidVM(
+                new Fluid("new-fluid-" + _newCount)));
+            _newCount++;
+        }
+
+        private bool CanRemoveFluid()
+        {
+            return this.ItemList.Where(o => o.IsSelected).Any();
+        }
+
+        private void RemoveFluid()
+        {
+            var res = this.ItemList.Where(o => o.IsSelected).ToList();
+            foreach (var r in res)
+                this.ItemList.Remove(r);
         }
     }
 }
