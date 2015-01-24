98076dda6b9ffc1f98ffd69c3a6e84b11c8178fb
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-23 23:34:58 -0500

    Added Technology Prerequisite model and vm (Issue #51)

---
+++ FactorioModBuilder/Models/ProjectItems/Prototype/TechnologyPrerequisite.cs
@@ -0,0 +1,16 @@
+﻿using FactorioModBuilder.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.Models.ProjectItems.Prototype
+{
+    public class TechnologyPrerequisite : TreeItem<TechnologyPrerequisite>
+    {
+        public TechnologyPrerequisite() : base("Tech Prereq")
+        {
+        }
+    }
+}
---
+++ FactorioModBuilder/ViewModels/ProjectItems/Prototype/TechnologyPrerequisiteVM.cs
@@ -0,0 +1,30 @@
+﻿using FactorioModBuilder.Models.ProjectItems.Prototype;
+using FactorioModBuilder.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioModBuilder.ViewModels.ProjectItems.Prototype
+{
+    public class TechnologyPrerequisiteVM 
+        : ProjectItem<TechnologyPrerequisite, TechnologyPrerequisiteVM>
+    {
+        public override IEnumerable<Build.Data.DataUnit> CompilerData
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TechnologyPrerequisiteVM(TechnologyPrerequisite item)
+            : this(null, item)
+        {
+        }
+
+        public TechnologyPrerequisiteVM(TreeItemVMBase parent, 
+            TechnologyPrerequisite item)
+            : base(parent, item)
+        {
+        }
+    }
+}
