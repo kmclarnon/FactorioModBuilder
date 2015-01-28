a7c83b44bd7fff5796cdc6b96e197db4fc377248
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-28 01:25:01 -0500

    More UI work.  Added in MahApps for the Metro Window

--- FactorioModBuilder/View/Main/MainWindow.xaml.cs
+++ FactorioModBuilder/View/Main/MainWindow.xaml.cs
@@ -17,13 +17,14 @@ using FactorioModBuilder.Models;
 using FactorioModBuilder.Models.Main;
 using FactorioModBuilder.Build;
 using FactorioModBuilder.Build.Extensions;
+using MahApps.Metro.Controls;
 
 namespace FactorioModBuilder
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainVM ViewModel { get { return (MainVM)this.DataContext; } }
 
