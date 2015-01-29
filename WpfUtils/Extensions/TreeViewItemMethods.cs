2b5b299f73690cbd9cd921620c1eab2a47bd7351
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-29 16:42:02 -0500

    Added support for full line highlighting in the TreeView

---
+++ WpfUtils/Extensions/TreeViewItemMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfUtils.Extensions
+{
+    /// <summary>
+    /// Extension methods for TreeViewItems
+    /// </summary>
+    public static class TreeViewItemMethods
+    {
+        /// <summary>
+        /// Determines this TreeViewItem's depth in the visual tree
+        /// </summary>
+        /// <returns>The number of levels down that this TreeViewItem is in the visual tree</returns>
+        public static int GetDepth(this TreeViewItem item)
+        {
+            TreeViewItem parent;
+            while ((parent = GetParent(item)) != null)
+            {
+                return GetDepth(parent) + 1;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Determines the parent TreeViewItem or TreeView of the given TreeViewItem
+        /// </summary>
+        /// <param name="item">The TreeViewItem to find the parent for</param>
+        /// <returns>The TreeViewItem or TreeView that is the parent of the given TreeViewItem</returns>
+        public static TreeViewItem GetParent(TreeViewItem item)
+        {
+            var parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as TreeViewItem;
+        }
+    }
+}
---
+++ WpfUtils/ValueConverters/LeftMarginMultiplier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfUtils.Extensions;
+
+namespace WpfUtils.ValueConverters
+{
+    /// <summary>
+    /// Determines the left margin based on the indent legth and the TreeViewItem's depth
+    /// </summary>
+    public class LeftMarginMultiplier : BaseConverter
+    {
+        /// <summary>
+        /// The length per indent
+        /// </summary>
+        public double Length { get; set; }
+
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var item = value as TreeViewItem;
+            if (item == null)
+                return new Thickness(0);
+            return new Thickness(this.Length * item.GetDepth(), 0, 0, 0);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
