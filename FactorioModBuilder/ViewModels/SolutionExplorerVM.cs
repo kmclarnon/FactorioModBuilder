0de9badc9e650566513de18f791402f45234d14f
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-29 13:51:05 -0500

    Added Double Click behavior to allow for MVVM command binding

--- FactorioModBuilder/ViewModels/SolutionExplorerVM.cs
+++ FactorioModBuilder/ViewModels/SolutionExplorerVM.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WpfUtils;
 
 namespace FactorioModBuilder.ViewModels
@@ -33,6 +34,8 @@ namespace FactorioModBuilder.ViewModels
             set { this.SetProperty(value); }
         }
 
+        public ICommand OpenSelectedItemCmd { get { return this.GetCommand(this.OpenSelectedItem); } }
+
         public ObservableCollection<IMenuItemProvider> MenuItems { get; private set; }
 
         public ObservableCollection<SolutionVM> Solutions { get; private set; }
@@ -41,6 +44,11 @@ namespace FactorioModBuilder.ViewModels
         {
             this.MenuItems = new ObservableCollection<IMenuItemProvider>();
             this.Solutions = new ObservableCollection<SolutionVM>();
+        }
+
+        private void OpenSelectedItem()
+        {
+
         }
     }
 }
---
+++ WpfUtils/Behaviors/MouseDoubleClick.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfUtils.Behaviors
+{
+    /// <summary>
+    /// Behavior that allows binding an ICommand to the MouseDoubleClick event of a Control
+    /// </summary>
+    public class MouseDoubleClick
+    {
+        /// <summary>
+        /// ICommand binding property
+        /// </summary>
+        public static DependencyProperty CommandProperty =
+        DependencyProperty.RegisterAttached("Command",
+            typeof(ICommand),
+            typeof(MouseDoubleClick),
+            new UIPropertyMetadata(CommandChanged));
+
+        /// <summary>
+        /// ICommand paramaeter binding property
+        /// </summary>
+        public static DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached("CommandParameter",
+            typeof(object),
+            typeof(MouseDoubleClick),
+            new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// Handles setting the command property
+        /// </summary>
+        public static void SetCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(CommandProperty, value);
+        }
+
+        /// <summary>
+        /// Handles setting the command parameter property
+        /// </summary>
+        public static void SetCommandParameter(DependencyObject target, object value)
+        {
+            target.SetValue(CommandParameterProperty, value);
+        }
+
+        /// <summary>
+        /// Handles getting the command parameter property
+        /// </summary>
+        public static object GetCommandParameter(DependencyObject target)
+        {
+            return target.GetValue(CommandParameterProperty);
+        }
+
+        /// <summary>
+        /// Handles hooking and unhooking the MouseDoubleClick event
+        /// </summary>
+        private static void CommandChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            Control control = target as Control;
+            if (control != null)
+            {
+                if ((e.NewValue != null) && (e.OldValue == null))
+                {
+                    control.MouseDoubleClick += OnMouseDoubleClick;
+                }
+                else if ((e.NewValue == null) && (e.OldValue != null))
+                {
+                    control.MouseDoubleClick -= OnMouseDoubleClick;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles invoking the bounding command
+        /// </summary>
+        private static void OnMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            Control control = sender as Control;
+            if (control != null)
+            {
+                // get the command and the associated parameter and invoke the command
+                ICommand command = (ICommand)control.GetValue(CommandProperty);
+                object commandParameter = control.GetValue(CommandParameterProperty);
+                command.Execute(commandParameter);
+            }
+        }
+    }
+}
