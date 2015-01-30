428e9e9cbdf1ef1349c80fff8e4e911d47a79e93
Kevin McLarnon <mclarnon.k@gmail.com>
2015-01-30 02:43:16 -0500

    Added further Context Menu support in the Solution Explorer

--- FactorioModBuilder/ViewModels/ProjectItems/Prototype/Filters/SubGroupFilterVM.cs
+++ FactorioModBuilder/ViewModels/ProjectItems/Prototype/Filters/SubGroupFilterVM.cs
@@ -1,4 +1,5 @@
-﻿using FactorioModBuilder.ViewModels.Menu;
+﻿using FactorioModBuilder.Models.ProjectItems.Prototype;
+using FactorioModBuilder.ViewModels.Menu;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,48 @@ namespace FactorioModBuilder.ViewModels.ProjectItems.Prototype.Filters
             this.MenuItems.Add(new CategoryItem("Add",
                 new ClickableItem("New SubGroup", this.AddNewSubGroupVM),
                 new ClickableItem("New Filter", this.AddFilter)));
+            this.MenuItems.Add(new SeparatorItem());
+            this.MenuItems.Add(new ClickableItem("Cut", this.Cut));
+            this.MenuItems.Add(new ClickableItem("Copy", this.Copy));
+            this.MenuItems.Add(new ClickableItem("Paste", this.Paste, this.CanPaste));
+            this.MenuItems.Add(new ClickableItem("Delete", this.Delete));
+            this.MenuItems.Add(new ClickableItem("Rename", this.Rename));
         }
 
         private void AddNewSubGroupVM()
         {
-            this.AddChild(new SubGroupVM(new Models.ProjectItems.Prototype.SubGroup("New SubGroup " + _newCount)));
+            this.AddChild(new SubGroupVM(new SubGroup("New SubGroup " + _newCount)));
             _newCount++;
+        }
+
+        private void Cut()
+        {
+
+        }
+
+        private void Copy()
+        {
+
+        }
+
+        private void Paste()
+        {
+
+        }
+
+        private bool CanPaste()
+        {
+            return false;
+        }
+
+        private void Delete()
+        {
+
+        }
+
+        private void Rename()
+        {
+
         }
     }
 }
