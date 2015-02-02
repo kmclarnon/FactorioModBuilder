beb534bafc694f1a845dbc5e24cbdab127d06c15
Kevin McLarnon <mclarnon.k@gmail.com>
2015-02-02 17:16:40 -0500

    Added new behaviors to bind command to preview right click and treeview right click select

---
+++ WpfUtils/Behaviors/TreeViewRightClickSelect.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfUtils.Behaviors
+{
+    /// <summary>
+    /// Behavior that allows a treeview to select the node right-clicked on before the right click event
+    /// </summary>
+    public class TreeViewRightClickSelect
+    {
+        /// <summary>
+        /// The right click select dependency property
+        /// </summary>
+        public static readonly DependencyProperty RightClickSelectProperty =
+            DependencyProperty.RegisterAttached("RightClickSelect",
+                typeof(bool),
+                typeof(TreeViewRightClickSelect),
+                new UIPropertyMetadata(RightClickSelectChanged));
+
+        /// <summary>
+        /// Sets the right click select property value
+        /// </summary>
+        public static void SetRightClickSelect(DependencyObject target, bool value)
+        {
+            target.SetValue(RightClickSelectProperty, value);
+        }
+
+        /// <summary>
+        /// Handles hooking and unhooking the events required to handle right click selection
+        /// </summary>
+        public static void RightClickSelectChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            TreeView tree = target as TreeView;
+            if (tree != null)
+            {
+                if ((bool)e.NewValue && !(bool)e.OldValue)
+                    tree.PreviewMouseRightButtonDown += OnPreviewMouseRightButtonDown;
+                else if (!(bool)e.NewValue && (bool)e.OldValue)
+                    tree.PreviewMouseRightButtonDown -= OnPreviewMouseRightButtonDown;
+            }
+        }
+
+        /// <summary>
+        /// Handles focusing the item selected before the right click
+        /// </summary>
+        private static void OnPreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TreeViewItem treeViewItem = VisualUpwardSearch(e.OriginalSource as DependencyObject);
+
+            if (treeViewItem != null)
+            {
+                treeViewItem.Focus();
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Finds the TreeViewItem owner of the source object
+        /// </summary>
+        private static TreeViewItem VisualUpwardSearch(DependencyObject source)
+        {
+            while (source != null && !(source is TreeViewItem))
+                source = VisualTreeHelper.GetParent(source);
+
+            return source as TreeViewItem;
+        }
+    }
+}
