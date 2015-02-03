389c560d65115a51a64c2105d910eaf9f4059f35
Kevin McLarnon <mclarnon.k@gmail.com>
2015-02-02 22:42:23 -0500

    Added a debug convenience converter

---
+++ WpfUtils/Debug/DebugConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WpfUtils.ValueConverters;
+
+namespace WpfUtils.Debug
+{
+    public class DebugConverter : BaseConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return Binding.DoNothing;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
